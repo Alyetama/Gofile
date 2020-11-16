4a59abebe625fd203f9ddb11855d1f86a959a613
Alyetama <56323389+Alyetama@users.noreply.github.com>
2020-11-16 03:33:13 -0600

    update

--- add_account.py
+++ add_account.py
@@ -1,20 +1,26 @@
-import re
+#!/usr/bin/env python
+# coding: utf-8
+
 import os
 
-ans = input('Add an email address (y/n)? ')
 
-if ans.lower() == 'y':
+def add_account():
+    ans = input('Add an email address (y/n)? ')
+    if ans.lower() == 'y':
+        email_addr = input('Email Address: ')
 
-    email_addr = input('Email Address: ')
+        with open('gofile.py') as f:
+            
+            with open('gofile_.py', 'w') as f2:
+                for num, line in enumerate(f):
+                    if num == 22:
+                        f2.write(f"    email_addr = '{email_addr}'\n")
+                    else:
+                        f2.write(line)
 
-    with open('gofile.py') as f:
+        os.remove('gofile.py')
+        os.rename('gofile_.py', 'gofile.py')
 
-        with open('gofile_.py', 'w') as f2:
-            for num, line in enumerate(f):
-                if num == 22:
-                    f2.write(f"    email_addr = '{email_addr}'\n")
-                else:
-                    f2.write(line)
 
-    os.remove('gofile.py')
-    os.rename('gofile_.py', 'gofile.py')
+if __name__ == '__main__':
+    add_account()
